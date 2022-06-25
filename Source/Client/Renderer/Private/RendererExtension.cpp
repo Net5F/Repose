3ea12cb64ab906c8ffd4fa6ec706fa328be13e61
Michael Puskas <mmpuskas@gmail.com>
2022-06-25 16:35:38 -0700

    Added placeholder RendererExtension.

---
+++ Source/Client/Renderer/Private/RendererExtension.cpp
@@ -0,0 +1,27 @@
+#include "RendererExtension.h"
+#include "Camera.h"
+#include "Log.h"
+
+namespace AM
+{
+namespace Client
+{
+
+RendererExtension::RendererExtension([[maybe_unused]] RendererExDependencies deps)
+{
+}
+
+void RendererExtension::beforeWorld([[maybe_unused]] const Camera& lerpedCamera, [[maybe_unused]] double alpha)
+{
+}
+
+void RendererExtension::afterWorld([[maybe_unused]] const Camera& lerpedCamera, [[maybe_unused]] double alpha) {
+}
+
+bool RendererExtension::handleOSEvent([[maybe_unused]] SDL_Event& event)
+{
+    return false;
+}
+
+} // End namespace Client
+} // End namespace AM
---
+++ Source/Client/Renderer/Public/RendererExtension.h
@@ -0,0 +1,45 @@
+#pragma once
+
+#include "IRendererExtension.h"
+#include "RendererExDependencies.h"
+
+namespace AM
+{
+struct Camera;
+
+namespace Client
+{
+
+/**
+ * PLACEHOLDER - Currently not rendering anything at the project level.
+ */
+class RendererExtension : public IRendererExtension
+{
+public:
+    // Note: This is the canonical constructor, expected by the factory that 
+    //       constructs this class. Do not modify it.
+    RendererExtension(RendererExDependencies deps);
+
+    /**
+     * Called before tiles and entities are rendered.
+     */
+    void beforeWorld(const Camera& lerpedCamera, double alpha) override;
+
+    /**
+     * Called after tiles and entities are rendered.
+     */
+    void afterWorld(const Camera& lerpedCamera, double alpha) override;
+
+    /**
+     * See OSEventHandler for details.
+     * 
+     * Note: Renderer will pass events to this class first. If the event is 
+     *       not handled, then Renderer will attempt to handle it.
+     */
+    bool handleOSEvent(SDL_Event& event) override;
+
+private:
+};
+
+} // End namespace Client
+} // End namespace AM
