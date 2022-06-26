9b36d454b8e58447cced197fb090b741b4dbf73e
Michael Puskas <mmpuskas@gmail.com>
2022-06-25 20:07:08 -0700

    Added placeholder SimulationExtension.

---
+++ Source/Client/Simulation/Private/SimulationExtension.cpp
@@ -0,0 +1,26 @@
+#include "SimulationExtension.h"
+#include "Camera.h"
+#include "Log.h"
+
+namespace AM
+{
+namespace Client
+{
+
+SimulationExtension::SimulationExtension([[maybe_unused]] SimulationExDependencies deps)
+{
+}
+
+void SimulationExtension::beforeAll() {}
+
+void SimulationExtension::afterMapAndLifetimeUpdates() {}
+
+void SimulationExtension::afterMovement() {}
+
+bool SimulationExtension::handleOSEvent([[maybe_unused]] SDL_Event& event)
+{
+    return false;
+}
+
+} // End namespace Client
+} // End namespace AM
---
+++ Source/Client/Simulation/Public/SimulationExtension.h
@@ -0,0 +1,50 @@
+#pragma once
+
+#include "ISimulationExtension.h"
+#include "SimulationExDependencies.h"
+
+namespace AM
+{
+struct Camera;
+
+namespace Client
+{
+
+/**
+ * PLACEHOLDER - Currently not simulating anything at the project level.
+ */
+class SimulationExtension : public ISimulationExtension
+{
+public:
+    // Note: This is the canonical constructor, expected by the factory that 
+    //       constructs this class. Do not modify it.
+    SimulationExtension(SimulationExDependencies deps);
+
+    /**
+     * Called before any systems are ran.
+     */
+    void beforeAll();
+
+    /**
+     * Called after the tile map is updated and NPCs are added/removed.
+     */
+    void afterMapAndLifetimeUpdates();
+
+    /**
+     * Called after all entity movement has been processed.
+     */
+    void afterMovement();
+
+    /**
+     * See OSEventHandler for details.
+     * 
+     * Note: Simulation will pass events to this class first. If the event is 
+     *       not handled, then Simulation will attempt to handle it.
+     */
+    bool handleOSEvent(SDL_Event& event) override;
+
+private:
+};
+
+} // End namespace Client
+} // End namespace AM
